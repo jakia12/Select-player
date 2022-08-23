a11498edd8c957bfa8a4521e64ad9d228bf659d2
jakia12 <owasi110@gmail.com>
2022-08-23 11:09:31 +0600

    all calculation added

--- js/calculate.js
+++ js/calculate.js
@@ -2,13 +2,12 @@
 
 const calculateBtn = document.getElementsByClassName('calc_btn')[0];
 
+//calculate the perplayer total once clicking the calculate button
 
-let result = '';
+let total = "";
 calculateBtn.addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
-
-
 
     const playerLen = document.getElementsByClassName('player_len')[0].innerText;
 
@@ -16,32 +15,70 @@ calculateBtn.addEventListener('click', function (e) {
 
     const playerInput = document.getElementById('perPlayerInput');
 
-    const playerInputVal = parseFloat(playerInput.value);
+    const playerInputVal = parseInt(playerInput.value);
 
+    if (isNaN(playerInput.value)) {
+        alert('Please provide a number');
+        playerInputVal.value = '';
 
-    const perPlayerExp = playerInputVal * playerLen;
-    // console.log(perPlayerExp);
+    } else {
+        const perPlayerExp = playerInputVal * playerLen;
+        // console.log(perPlayerExp);
 
-    const totalAmount = document.getElementById('totalAmount');
+        const totalAmount = document.getElementById('totalAmount');
 
-    result += perPlayerExp;
-    totalAmount.innerText = result;
+        //push the data inside of the span element
+        totalAmount.innerText = perPlayerExp;
 
+        playerInput.value = '';
+    }
 
-    playerInput.value = '';
-
-    return result;
+    //store the total expense to use later on
+    total += totalAmount.innerText;
 
 });
 
 
 //all expenses calculation together
 
-const calcTotal = document.getElementById('calctotal');
+const calcTotalBtn = document.getElementById('calctotal');
 
-calcTotal.addEventListener('click', function (e) {
+//calculate all the expenses total once clicking the calculate total button
+
+calcTotalBtn.addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
+
+    //get the manager input value
+    const managerInput = document.getElementById('managerInput');
+    const managerInputVal = parseFloat(managerInput.value);
+
+    //get the coach input value
+    const coachInput = document.getElementById('coachInput');
+    const coachInputval = parseFloat(coachInput.value);
+
+    //validate the input value
+    if (managerInputVal !== 'number') {
+        alert('Please provide a number');
+        managerInput.value = '';
+
+    } else if (coachInputval !== 'number') {
+        alert('Please provide a number');
+        managerInput.value = '';
+
+    } else {
+        //get the span element by id
+        const expensesTotal = document.getElementById('expensesTotal');
+
+        //all the total expenses's sum
+        const totalExpensesAmount = parseFloat(total) + managerInputVal + coachInputval;
+
+        expensesTotal.innerText = totalExpensesAmount;
+
+        managerInput.value = '';
+        coachInput.value = '';
+    }
+
 
 
 
